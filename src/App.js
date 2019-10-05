132d061ad99eb050212c04565b409e8b0b68b4b8
dusica982 <dusica.antic@gmail.com>
2019-10-05 23:10:46 +0200

    making navbar

--- src/App.js
+++ src/App.js
@@ -8,6 +8,27 @@ function App() {
       <header className="App-header">
         <p>TO DO OR NOT TO DO</p>
       </header>
+
+      <div className="mainPartOfThePage">
+        <div className="leftPart">
+          <ul>
+            <li>
+              <a href="#">Boards</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">New Board</a>
+            </li>
+          </ul>
+        </div>
+        <ul className="rightList">
+          <li className="rightPart">Your Boards</li>
+        </ul>
+      </div>
+
+      <footer>Powered by Geckos Team 8</footer>
     </div>
   );
 }
